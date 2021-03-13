4ee79126e849bbef04b6fc6d84b76a6117c05d0a
HavWho <workplacer2019@gmail.com>
2021-03-13 22:41:10 +0300

    Main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
+import static com.company.Rails.*;
+
 public class Main {
 
+    public static void Vigener(String input) {
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input your string: ");
+        String StringForEncrypt = scanner.nextLine();
+
+        System.out.print("Input your key: ");
+        int key = scanner.nextInt();
+
+        System.out.println("Rails encryption method: ");
+        System.out.println(EncryptRails(StringForEncrypt, key));
+        System.out.println(DecryptRails(EncryptRails(StringForEncrypt, key), key));
     }
 }
