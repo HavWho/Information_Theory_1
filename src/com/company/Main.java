0d35eedfb2435119d6589d854990075d49d4cf91
HavWho <workplacer2019@gmail.com>
2021-03-18 18:52:51 +0300

    Main class with main function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 import static com.company.Rails.*;
 import static com.company.Column.*;
+import static com.company.Lattice.*;
+import static com.company.Vigener.*;
 
 public class Main {
-
-    public static void Vigener(String input) {
-
-    }
 
     public static void main(String[] args) {
         Scanner ScannerInt = new Scanner(System.in);
         Scanner ScannerString = new Scanner(System.in);
+        Scanner ScannerInt2 = new Scanner(System.in);
 
         System.out.print("Input your string: ");
         String StringForEncrypt = ScannerInt.nextLine();
@@ -36,5 +35,37 @@ public class Main {
         System.out.println("Column encryption method: ");
         System.out.println(EncryptColumn(StringForEncrypt, StringKey));
         System.out.println("decryption: ");
+        System.out.println(DecryptColumn(EncryptColumn(StringForEncrypt, StringKey), StringKey));
+
+        System.out.println("\n");
+
+        boolean flag = true;
+
+        do {
+            int n;
+            n = ScannerInt2.nextInt();
+
+            if (n % 2 == 0 && n * n >= StringForEncrypt.length()) {
+
+                flag = false;
+
+                int[][] MatrixForLattice = new int[n][n];
+                MatrixForLattice = latticeInput(n);
+
+                System.out.println("Rotating Lattice encryption method: ");
+                System.out.println(EncryptLattice(StringForEncrypt, n, MatrixForLattice));
+                System.out.println("decryption: ");
+                System.out.println(DecryptLattice(EncryptLattice(StringForEncrypt, n, MatrixForLattice), n, MatrixForLattice));
+            } else {
+                System.out.println("Incorrect input!");
+            }
+        } while(flag);
+
+        System.out.println("\n");
+
+        System.out.println("Vigener encryption method: ");
+        System.out.println(EncryptVigener(StringForEncrypt, StringKey));
+        System.out.println("decryption: ");
+        System.out.println(DecryptVigener(EncryptVigener(StringForEncrypt, StringKey), StringKey));
     }
 }
